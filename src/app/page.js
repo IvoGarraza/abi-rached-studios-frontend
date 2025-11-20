0f0998507fe556ffb95808518ca3c9b420a30a56
IvoGarraza <ivogarrachip@gmail.com>
2025-11-20 12:02:36 -0300

    ultimos ajustes de forms

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,6 @@ import Hero from "@/sections/Hero/Hero";
 import Loader from "@/components/loader/Loader.jsx"
 import Bento from "@/sections/Bento/Bento";
 import Talents from "@/sections/Talents/Talents";
-import Login from "@/components/Forms/LoginForm";
-import Register from "@/components/Forms/RegisterForm";
-import Apply from "@/components/Forms/ApplyForm";
 import Contact from "@/sections/Contact/Contact";
 
 export default function Home() {
@@ -17,9 +14,6 @@ export default function Home() {
       <Hero></Hero>
       <Bento></Bento>
       <Talents></Talents>
-      <Login></Login>
-      <Register></Register>
-      <Apply></Apply>
       <Contact></Contact>
     </div>
   );
